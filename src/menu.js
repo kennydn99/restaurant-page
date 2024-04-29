358a9b54254433bd7bb3b564f7676a637f8ced56
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-04-29 14:16:28 -0700

    Add menu images and finish styling

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,7 @@
+import PepperBeefImage from './Beef-Pepper-Fry.jpg';
+import BeefCutletImage from './beef-cutlet.jpg';
+import DosaImage from './dosa.jpg';
+
 const createMenuPage = () => {
     const content = document.querySelector('#content');
     const pageContent = document.createElement('div');
@@ -8,11 +12,26 @@ const createMenuPage = () => {
 
     const menuList = document.createElement('ul');
     const menuItem = document.createElement('li');
-    menuItem.textContent = "Pepper Beef";
+    menuItem.textContent = "Beef Pepper Fry";
+    const itemImage = document.createElement('img');
+    itemImage.classList.add('menu-img');
+    itemImage.src = PepperBeefImage;
+    menuItem.appendChild(itemImage);
+
     const menuItem2 = document.createElement('li');
     menuItem2.textContent = "Beef Cutlet";
+    const itemImage2 = document.createElement('img');
+    itemImage2.classList.add('menu-img');
+    itemImage2.src = BeefCutletImage;
+    menuItem2.appendChild(itemImage2);
+
     const menuItem3 = document.createElement('li');
-    menuItem3.textContent = "Dosa";
+    menuItem3.textContent = "Masala Dosa";
+    const itemImage3 = document.createElement('img');
+    itemImage3.classList.add('menu-img');
+    itemImage3.src = DosaImage;
+    menuItem3.appendChild(itemImage3);
+
     menuList.appendChild(menuItem);
     menuList.appendChild(menuItem2);
     menuList.appendChild(menuItem3);
